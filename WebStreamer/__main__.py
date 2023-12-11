879a41232f3d3ff192c3ee0206c6aa2685461aeb
Hooley Mass <hockleymasese3@gmail.com>
2023-12-11 23:11:21 +0300

    sync

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -61,6 +61,11 @@ if __name__ == "__main__":
     except Exception as err:
         logging.error(err.with_traceback(None))
     finally:
-        loop.run_until_complete(cleanup())
-        loop.stop()
-        logging.info("Stopped Services")
+        try:
+            loop.run_until_complete(cleanup())
+        except Exception as err:
+            logging.error(err.with_traceback(None))
+        finally:
+            loop.stop()
+            logging.info("Stopped Services")
+
