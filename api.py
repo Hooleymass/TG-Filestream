755217e21b170bf45d02d69634ca04149cc130c3
Hooley Mass <hockleymasese3@gmail.com>
2023-12-12 12:09:47 +0300

    api sorted

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Load the generated URLs from the JSON file
-with open("generated_urls.json", "r") as json_file:
-    generated_urls = json.load(json_file)
+#with open("generated_urls.json", "r") as json_file:
+#    generated_urls = json.load(json_file)
 
 
 # Flask route to serve the generated URLs
