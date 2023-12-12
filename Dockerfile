07201fd976d503d4d0eb91d37b701a876c5442c5
Hooley Mass <hockleymasese3@gmail.com>
2023-12-12 11:45:52 +0300

    separate api

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 CMD ["python3","-m","WebStreamer"]
+CMD ["python3", "main.py"]
--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -1,7 +1,9 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
 import sys
 import asyncio
 import logging
-import json
 from .vars import Var
 from aiohttp import web
 from pyrogram import idle
@@ -9,7 +11,7 @@ from WebStreamer import utils
 from WebStreamer import StreamBot
 from WebStreamer.server import web_server
 from WebStreamer.bot.clients import initialize_clients
-from flask import Flask, jsonify  # Added Flask import
+
 
 logging.basicConfig(
     level=logging.DEBUG if Var.DEBUG else logging.INFO,
@@ -22,13 +24,10 @@ logging.getLogger("aiohttp").setLevel(logging.DEBUG if Var.DEBUG else logging.ER
 logging.getLogger("pyrogram").setLevel(logging.INFO if Var.DEBUG else logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.DEBUG if Var.DEBUG else logging.ERROR)
 
-# Added list to store generated URLs
-generated_urls = []
-
 server = web.AppRunner(web_server())
-app = Flask(__name__)  # Initialize Flask app
 
 loop = asyncio.get_event_loop()
+
 
 
 async def start_services():
@@ -48,38 +47,15 @@ async def start_services():
     if bot_info.dc_id:
         logging.info("DC ID =>> {}".format(str(bot_info.dc_id)))
     logging.info("URL =>> {}".format(Var.URL))
-
-    # Append the generated URL to the list
-    generated_urls.append(Var.URL)
-
     await idle()
-
 
 async def cleanup():
     await server.cleanup()
     await StreamBot.stop()
 
-    # Save the list of generated URLs to a JSON file
-    with open("generated_urls.json", "w") as json_file:
-        json.dump(generated_urls, json_file, indent=2)
-
-
-# Flask route to serve the generated URLs
-@app.route('/api/generated_urls', methods=['GET'])
-def get_generated_urls():
-    return jsonify(generated_urls)
-
-
 if __name__ == "__main__":
     try:
-        # Run Flask app in a separate thread
-        import threading
-        flask_thread = threading.Thread(target=app.run, kwargs={'port': 8080, 'debug': False})
-        flask_thread.start()
-
-        # Run asyncio loop for the main functionality
         loop.run_until_complete(start_services())
-
     except KeyboardInterrupt:
         pass
     except Exception as err:
